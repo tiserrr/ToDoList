ca79970e18ecc248f4e239f5cd30f6bd91d92c53
G2-01-UA <savchuk000v@gmail.com>
2022-02-15 13:29:21 +0200

    Moving forward

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,11 @@
 from unicodedata import name
 from django.urls import path
-from .views import TaskDetail, TaskList
+from .views import TaskCreate, TaskDelete, TaskDetail, TaskList, TaskUpdate, DeleteView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>', TaskDetail.as_view(), name='task')
+    path('task/<int:pk>', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task-edit/<int:pk>', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>', TaskDelete.as_view(), name='task-delete')
 ]
