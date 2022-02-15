c62f8382464b26bc977b57cecbaf52f9f17843bf
G2-01-UA <savchuk000v@gmail.com>
2022-02-15 12:50:59 +0200

    new file:   base/__init__.py 	new file:   base/__pycache__/__init__.cpython-38.pyc 	new file:   base/__pycache__/admin.cpython-38.pyc 	new file:   base/__pycache__/apps.cpython-38.pyc 	new file:   base/__pycache__/models.cpython-38.pyc 	new file:   base/__pycache__/urls.cpython-38.pyc 	new file:   base/__pycache__/views.cpython-38.pyc 	new file:   base/admin.py 	new file:   base/apps.py 	new file:   base/migrations/0001_initial.py 	new file:   base/migrations/__init__.py 	new file:   base/migrations/__pycache__/0001_initial.cpython-38.pyc 	new file:   base/migrations/__pycache__/__init__.cpython-38.pyc 	new file:   base/models.py 	new file:   base/templates/base/task_detail.html 	new file:   base/templates/base/task_form.html 	new file:   base/templates/base/task_list.html 	new file:   base/tests.py 	new file:   base/urls.py 	new file:   base/views.py 	new file:   db.sqlite3 	new file:   todo_list/__pycache__/__init__.cpython-38.pyc 	new file:   todo_list/__pycache__/settings.cpython-38.pyc 	new file:   todo_list/__pycache__/urls.cpython-38.pyc 	new file:   todo_list/__pycache__/wsgi.cpython-38.pyc 	modified:   todo_list/settings.py 	modified:   todo_list/urls.py

---
+++ base/urls.py
@@ -0,0 +1,8 @@
+from unicodedata import name
+from django.urls import path
+from .views import TaskDetail, TaskList
+
+urlpatterns = [
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>', TaskDetail.as_view(), name='task')
+]
---
+++ base/views.py
@@ -0,0 +1,21 @@
+from asyncio import Task
+from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from .models import Task
+
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    
