2e90452f3dd995ec632e23683583434c8bf7b246
G2-01-UA <savchuk000v@gmail.com>
2022-02-16 13:09:01 +0200

    Almost there

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,12 @@
 from unicodedata import name
 from django.urls import path
-from .views import TaskCreate, TaskDelete, TaskDetail, TaskList, TaskUpdate, TaskDelete, CustomLoginView
+from .views import TaskCreate, TaskDelete, TaskDetail, TaskList, TaskUpdate, TaskDelete, CustomLoginView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page = 'login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
 
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>', TaskDetail.as_view(), name='task'),
